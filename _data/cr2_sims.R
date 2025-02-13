7e4c56a54755866b2e059caa04a24033767c8327
gerasy1987 <gerasy@gmail.com>
2025-02-12 22:40:33 -0600

    update experiments slides

--- _data/cr2_sims.R
+++ _data/cr2_sims.R
@@ -48,8 +48,12 @@ simulate <- function(rel, sims = 100) {
 
       c(
         rel = rel,
-        HC2 = unname(tau >= naive_model$conf.low[2] & tau <= naive_model$conf.high[2]),
-        CR2 = unname(tau >= robust_se$conf.low[2] & tau <= robust_se$conf.high[2])
+        HC2 = unname(
+          tau >= naive_model$conf.low[2] & tau <= naive_model$conf.high[2]
+        ),
+        CR2 = unname(
+          tau >= robust_se$conf.low[2] & tau <= robust_se$conf.high[2]
+        )
       )
     }
   ) |>
@@ -57,33 +61,44 @@ simulate <- function(rel, sims = 100) {
 }
 
 # Run simulations
-results <- 
+results <-
   bind_rows(pbapply::pblapply(seq(0.05, 2, by = .05), simulate, cl = 8))
 
 write_rds(results, "_data/cr2_sims.rds")
 
+results <- read_rds("_data/cr2_sims.rds")
+
 # Prepare data for animation
-results_long <- 
-  results |> 
-  pivot_longer(cols = c("HC2", "CR2"), names_to = "method", values_to = "se")
+results_long <-
+  results |>
+    pivot_longer(cols = c("HC2", "CR2"), names_to = "method", values_to = "se")
 
 # Plot animation
-p <- 
+p <-
   ggplot(results_long, aes(x = se, y = rel, color = method, shape = method)) +
-  geom_hline(aes(yintercept = rel), linetype = "dashed", alpha = .5, size = .25) +
-  geom_point(size = 4, alpha = 1) +
+    geom_hline(
+      aes(yintercept = rel),
+      linetype = "dashed",
+      alpha = .5,
+      size = .25
+    ) +
+    geom_point(size = 4, alpha = 1) +
     labs(
-      title = , 
+      title = ,
       x = "Coverage",
       color = "Std. Errors",
       shape = "Std. Errors",
-      y = bquote("Ratio of Between to Within SD (" ~sigma[B]/sigma[W] ~ ")")) +
-  scale_x_continuous(breaks = seq(0.45,1, by = 0.05)) +
-  scale_color_manual(values = c("#98971a", "#cc241d")) +
-  theme_bw() +
-  transition_components(rel)
+      y = bquote("Ratio of Between to Within SD (" ~ sigma[B] / sigma[W] ~ ")")
+    ) +
+    scale_x_continuous(breaks = seq(0.45, 1, by = 0.05)) +
+    scale_color_manual(values = c("#98971a", "#cc241d")) +
+    theme_bw() +
+    transition_components(rel)
 
 # Save animation
-gganimate::animate(p, 
-  renderer = gifski_renderer(file = "_images/cr2_coverage.gif"), 
-  fps = 15, duration = 10, rewind = TRUE)
+gganimate::animate(
+  p,
+  renderer = gifski_renderer(file = "_images/cr2_coverage.gif"),
+  fps = 15,
+  duration = 15
+)
